309256322de62a9cd61414da3ca188787eb53d29
tjoeunjr5-dev <tjoeun.jr5@gmail.com>
2026-03-03 13:01:04 +0900

    board

--- spring-board/src/main/java/com/board/springboard/controller/ViewController.java
+++ spring-board/src/main/java/com/board/springboard/controller/ViewController.java
@@ -1,4 +1,15 @@
 package com.board.springboard.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // jsp 나 html 템플릿과 소비자가 연결해야하는 api 주소 작성
 public class ViewController {
+
+    @GetMapping("/")
+    public String indexView(){
+        return "index";
+    }
+
 }
