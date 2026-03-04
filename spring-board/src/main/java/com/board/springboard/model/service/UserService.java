d1c9aaed1506d010e295e7819251de6c7a32a0cc
tjoeunjr5-dev <tjoeun.jr5@gmail.com>
2026-03-04 11:57:24 +0900

    users

---
+++ spring-board/src/main/java/com/board/springboard/model/service/UserService.java
@@ -0,0 +1,58 @@
+package com.board.springboard.model.service;
+
+import com.board.springboard.model.dto.User;
+import com.board.springboard.model.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserMapper userMapper;
+
+    /**
+     *
+     * @param email
+     * @return
+     */
+    public boolean 이메일중복체크기능(String email){
+       /*
+        int 이메일중복체크(String email); -> 결과는 count에 해당하는 숫자로 나온다.
+
+        하지만 이메일중복인지 아닌지에 대하여 true false 로 전달할 것임 설정
+        반환값이 int 이므로 중복체크기능으로 전달해야하는 데이터 자료형과
+        전달하겠다 선언한 자료형이 현재 다른 상황
+
+        이러할 경우에는 개발자가 컨트롤러에 어떻게 데이터를 전달할 것인가?
+        기준에 따라 return 옆에 작성한 기능 또는 public 옆에 작성한 자료형 변경
+
+        return userMapper.이메일중복체크(email);
+        */
+
+        /* 만약 이메일 중복체크를 SQL에서 진행했는데, 이메일이 1개이상발견된다면
+        이메일이 존재하는 것으로 확인
+
+        이메일이 1개 이상 없을 경우 이메일이 없습니다. false 의 형태
+         */
+        return userMapper.이메일중복체크(email) > 0; // 이메일이 존재하면 true 존재합니다!
+    }
+
+    /**
+     *
+     * @param user
+     * @return
+     */
+    public boolean 회원가입(User user) {
+        // 이메일이 중복일 경우 회원가입을 할 수 없도록 회원가입 중단
+        // 18 번 째 줄에서 개발자가 만들어놓은 이메일 존재 유무 확인하는 기능을 사용해서
+        // 만약에 소비자가 작성한 이메일이 존재하는게 사실이라면
+        if(이메일중복체크기능(user.getEmail())) {
+            return false; // 회원가입을 여기서 중단하겠어요.^^ 회원가입 못하겠네요. 를 반환
+        }
+        // 이메일 중복체크기능이 false 이고 이메일이 sql에 존재하지 않는게 사실이라면
+        // 회원가입을 진행하고
+        userMapper.회원가입(user);
+        return true; // sql 에 저장이 완료되었다면 회원가입 완료를 클라이언트에게 전달하겠다.
+    }
+
+}
