281653fa426037f75e85aa523684b27776e11119
tjoeunjr5-dev <tjoeun.jr5@gmail.com>
2026-03-09 12:41:27 +0900

    board 이미지 단일 업로드

--- spring-board/src/main/java/com/board/springboard/model/service/BoardService.java
+++ spring-board/src/main/java/com/board/springboard/model/service/BoardService.java
@@ -4,9 +4,11 @@ package com.board.springboard.model.service;
 import com.board.springboard.model.dto.Board;
 import com.board.springboard.model.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,6 +18,9 @@ public class BoardService {
     // 보드 매퍼 에 작성된 기능을 활용하겠다.
     // 보드 매퍼 는 xml에서 가져온 SQL 기능을 보유하고 있는 명칭들의 집합소!
     private final BoardMapper boardMapper;
+
+    @Value("${file.upload.board-path}")
+    private String boardUploadPath; // application.yml -> config.yml 에서 가져오는 경로
 
     // 전체게시물 조회
     public List<Board> findAllBoard(){
@@ -33,8 +38,9 @@ public class BoardService {
     // 게시물 추가 됐는지 유무만 확인
     public void writeBoard(Board board, MultipartFile imageFile, String uploadPath) throws IOException {
       // TODO 1 : 만약 이미지 파일이 있을 경우에만 컬럼 데이터 업로드
-
+        if(imageFile != null & !imageFile.isEmpty()) {
         // TODO 2 : 회사컴퓨터 or 개발자컴퓨터에 저장 폴더 없으면 자동 생성
+            File folder = new File(boardUploadPath);
 
         // TODO 3 : UUID + 원본 확장자로 파일명 생성 (중복파일명 충돌 방지)
 
@@ -43,7 +49,7 @@ public class BoardService {
         // TODO 5 : DB에 저장할 웹 접근 경로를 Board 객체 세팅
         // 실제 회사컴퓨터 or 개발자컴퓨터에 올라가는 접근 경로 uploads/board
         // 웹 접근 경로 : board
-
+        }
         // TODO 6 : DB에 게시물 저장 (이미지 없으면 attach_img = null 로 저장되며, 수정할 일 없음)
         boardMapper.게시물추가(board);
 
