81a75f5d30a58d5e5051dce9fa115c030bc97ecd
tjoeunjr5-dev <tjoeun.jr5@gmail.com>
2026-03-11 12:06:36 +0900

    POST -> put, or patch로 교체작업

--- spring-board/src/main/java/com/board/springboard/controller/ViewController.java
+++ spring-board/src/main/java/com/board/springboard/controller/ViewController.java
@@ -7,9 +7,7 @@ import com.board.springboard.model.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -181,10 +179,12 @@ public class ViewController {
         return "board/edit";
     }
 
-    @PostMapping("/board/edit")
-    public String editBoard(Board board) {
+    @PutMapping("/board/edit")
+    @ResponseBody
+    public Board editBoard(Board board) {
         boardService.updateBoard(board);
-        return "redirect:/board/detail?no=" + board.getBoard_no();
+        return board;
+    //    return "redirect:/board/detail?no=" + board.getBoard_no();
     }
 
     /**
