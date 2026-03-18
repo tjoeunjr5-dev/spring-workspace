56b07c7998d2a20a0a81b1959bd110b372ef6ed2
tjoeunjr5-dev <tjoeun.jr5@gmail.com>
2026-03-18 11:22:47 +0900

    view Controller 와 API Controller 구분하여 코드 정리 seesion 형태로 저장한 유저 정보를 JWT 토큰으로 저장된 유저 정보 조회로 교체

--- spring-board/src/main/java/com/board/springboard/controller/UserApiController.java
+++ spring-board/src/main/java/com/board/springboard/controller/UserApiController.java
@@ -1,9 +1,13 @@
 package com.board.springboard.controller;
 
+import com.board.springboard.common.CookieUtil;
 import com.board.springboard.model.dto.User;
 import com.board.springboard.model.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,45 @@ import java.util.Map;
 public class UserApiController {
 
     private final UserService userService;
+    private final CookieUtil cookieUtil;
+    @Value("${file.upload.path}") // org.springframework.beans.factory.annotation.Value;
+    private String uploadPath;
+
+    @PostMapping("/user/check-email")
+    public ResponseEntity<?> 이메일중복확인(@RequestBody Map<String, String> body) {
+        boolean 중복 = userService.이메일중복체크기능(body.get("email"));
+        return ResponseEntity.ok(Map.of("duplicate", 중복)); //이메일 중복했는데 중복이 맞다면 duplicate - 중복 전달
+    }
+
+    @PostMapping("/user/send-code)
+    public ResponseEntity<?> 인증번호발송(@RequestBody Map<String, String> body) {
+        userService.인증번호발송(body.get("email"));
+        return ResponseEntity.ok(Map.of("message", "인증번호가 발송되었습니다."));
+    }
+
+    @PostMapping("/user/check-email")
+    public ResponseEntity<?> 인증번호확인(@RequestBody Map<String, String> body) {
+        boolean 성공 = userService.인증번호검증(body.get("email"), body.get("code"));
+        if (!성공) return ResponseEntity.badRequest().body(Map.of("message", "인증번호가 올바르지 않습니다."));
+        return ResponseEntity.ok(Map.of("message", "인증 성공"));
+    }
+
+    @PostMapping("/user/register")
+    public ResponseEntity<?> 회원가입(@RequestBody User user) {
+        boolean 성공 = userService.회원가입(user);
+        if (!성공) return ResponseEntity.badRequest().body(Map.of("message", "이미 사용중인 이메일입니다."));
+        return ResponseEntity.ok(Map.of("message", "회원가입 완료"));
+    }
+
+    @PostMapping("/user/login")
+    public ResponseEntity<?> 로그인(@RequestBody Map<String, String> body,
+                                 HttpServletResponse response) {
+        Map<String, String> 토큰들 = userService.로그인(body.get("email"), body.get("password"));
+        if (토큰들==null) return ResponseEntity.badRequest().body(Map.of("message", "이메일 또는 비밀번호가 올바르지 않습니다."));
+        cookieUtil.추가(response, "access_token", 토큰들.get("accessToken"), 60 * 30);
+        cookieUtil.추가(response, "refresh_token", 토큰들.get("refreshToken"), 60 * 30 * 24 * 14);
+        return ResponseEntity.ok(Map.of("message", "로그인 성공"));
+    }
 
     @PostMapping("/user/find-email")
     public Map<String, String> findEmail(@RequestBody Map<String, String> requestData) {
@@ -31,10 +74,7 @@ public class UserApiController {
     // profile.jsp 의 버튼 onclick → fetch() → JSON 받아서 성공 메세지 표시
     @PostMapping("/user/profile/edit")
     public Map<String, String> 유저정보수정(@RequestBody User user, HttpSession session) {
-        // 힌트1: 세션에서 로그인 유저 꺼내기
-        // 힌트2: 로그인 안 된 경우 에러 메세지 JSON 반환
-        // 힌트3: user 에 세션에서 꺼낸 id 세팅 후 수정 처리
-        // 힌트4: 세션 최신 정보로 갱신 후 성공 메세지 JSON 반환
+
         User 로그인유저 = (User) session.getAttribute("loginUser");
         if (로그인유저 == null) return Map.of("error", "로그인이 필요합니다.");
         user.setId(로그인유저.getId());
